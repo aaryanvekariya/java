65d9ed90e5447011a8009ddf1785f20e56ad54e6
aaryanvekariya <aaryanvekariya0143@gmail.com>
2025-05-15 09:35:00 +0530

    Update in BubbleSort.java file

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,31 +1,42 @@
+package Sorting;
+import java.util.Scanner;
+class BubbleSort {
+    // Bubble Sort Algorithm
+    // Time Complexity: O(n^2)
+    // Space Complexity: O(1)
+    // Stable Sort
+    // In-place sorting algorithm
 
-import java.util.*;
+    // This is a simple implementation of the bubble sort algorithm in Java.
+    // It sorts an array of integers in ascending order.
 
-public class BubbleSort { 
+    // The algorithm repeatedly steps through the list, compares adjacent elements,
+    // and swaps them if they are in the wrong order. The pass through the
+public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {  
-            for (int j = 0; j < n - 1 - i; j++) {  
-                if (arr[j] > arr[j + 1]) // for descending order, change to < 
-                
-                {
-                    // Swap the elements
+        int arr[] = new int[n];
+
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+            System.out.println("=======After Sorting=======");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
                     int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
-    }
 
-    public static void main(String[] args) {
-        
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
-        System.out.println("Original Array: " + Arrays.toString(arr));
-        
-        bubbleSort(arr); 
-        
-        System.out.println("Sorted Array: " + Arrays.toString(arr));
+        for(int i =0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+
+
     }
-}
+}    
